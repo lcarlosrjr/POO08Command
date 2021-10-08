37b80d9ff15561d6a6711d79fd88feb2dda5833e
joaoBatistaDesenvolvedor <joaobspaula@outlook.com>
2021-10-08 09:32:01 -0300

    Pratica 8 Command

---
+++ Pratica08Command/src/DocumentInvoker.java
@@ -0,0 +1,25 @@
+import java.io.Console;
+import java.util.ArrayList;
+
+public class DocumentInvoker{
+
+	private ArrayList<Command> commands=new ArrayList<Command>();
+	private Document doc=new Document();
+	
+	public void redo(int level) {
+		Console.WriteLine();
+		
+	}
+	
+	public void undo (int level) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void write(String text) {
+		
+	}
+	public void erase(String text) {
+		
+	}
+	
+}
